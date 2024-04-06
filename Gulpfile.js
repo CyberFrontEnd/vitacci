f2a6992af1035a8351d20596fad72a39ab3e0ff5
CyberFrontEnd <mail.techcd.ru>
2024-04-06 14:18:23 +0400

    new page 06,04,2024

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,9 @@
 var gulp = require('gulp');
 var loadPlugins  = require('gulp-load-plugins')();
 var browserSync  = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
 
 
 gulp.task('servers', function() {
@@ -20,17 +23,34 @@ gulp.task('html',function(){
 		.on('end', browserSync.reload);
 });
 
-gulp.task('css',function(){
-	return gulp.src('dist/css/*.css')
-		.pipe(gulp.dest('build/css'))
-		.pipe(browserSync.stream());
+// gulp.task('css',function(){
+// 	return gulp.src('dist/css/*.css')
+// 		.pipe(gulp.dest('build/css'))
+// 		.pipe(browserSync.stream());
+// });
+
+gulp.task('css', function() {
+	return gulp.src('dist/css/*.css') // Path to your CSS files
+		.pipe(concat('build/css/all.css')) // Combine all CSS files into one
+		.pipe(cleanCSS()) // Minify the CSS
+		.pipe(gulp.dest('build/css')) // Output directory for the minified CSS
+    .pipe(browserSync.stream());
 });
 
-gulp.task('js',function(){
-	return gulp.src('dist/js/*.js')
-		.pipe(gulp.dest('build/js'))
-		.on('end', browserSync.reload);
+
+gulp.task('js', function() {
+	return gulp.src('dist/js/*.js') // Path to your JavaScript files
+		.pipe(concat('build/all.js')) // Combine all JavaScript files into one
+		.pipe(uglify()) // Minify the JavaScript
+		.pipe(gulp.dest('build/js')) // Output directory for the minified JavaScript
+    .on('end', browserSync.reload);
 });
+
+// gulp.task('js',function(){
+// 	return gulp.src('dist/js/*.js')
+// 		.pipe(gulp.dest('build/js'))
+// 		.on('end', browserSync.reload);
+// });
 
 
 gulp.task('fonts',function(){
