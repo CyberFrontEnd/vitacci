36c532960bf3629d4c8c161b3ca7c4f61e78b164
CyberFrontEnd <mail.techcd.ru>
2024-04-10 14:15:38 +0400

    New Files

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,70 @@
 $(document).ready(function () {
+    $('.sliderBlockSlider').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      arrows: false,
+    });
+
+    $('.sliderItem').slick({
+      dots: false,
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrows: true,
+    });
+
+    $('.newsSlider').slick({
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+    });
+
+
+    // Start top slider main page
+    var $slider = $('.collectionTopLeftItem');
+    var $currentSlide = $('.current-slide');
+    var $totalSlides = $('.total-slides');
+
+    $slider.on('init afterChange', function(event, slick, currentSlide, nextSlide){
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      $currentSlide.text(i);
+      $totalSlides.text(slick.slideCount);
+    });
+
+    $slider.slick({
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 880,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+      ]
+    });
+  });
+
+// Start tabs setting
+$(document).ready(function () {
+  $('#tab-container').easytabs({
+    animationSpeed: 'fast',
+    updateHash: false,
+  });
+
+});
+
+
+$(document).ready(function () {
   // Start select
   $('.select-selected').on('click', function () {
     $('.select-items').not($(this).parents('.custom-select').children('.select-items')).slideUp();
